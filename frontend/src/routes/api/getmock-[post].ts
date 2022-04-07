af3229adc6b6f414098eecabfc6b46270d99afdd
smaristeinar <smairkul@hotmail.com>
2022-04-07 14:16:58 +0200

    feat: adds correct mock data

--- frontend/src/routes/api/getmock-[post].ts
+++ frontend/src/routes/api/getmock-[post].ts
@@ -1,40 +1,45 @@
-const mockdata: any = [
-      {
-        "id": "44608",
-        "thumbnail": {
-          "url": "https://www.datocms-assets.com/65130/1647527094-us-skateboarder-cory-juneau.jpg"
-        },
-        "createdAt": "2022-03-17T14:30:33Z",
-        "content": "jsjadjaslkjddddddddddddddddddddasjdklajskldjasdjaskljdkalsjdklajskdjaskljdklasjkljgkfjsakfnsdmklvjpsdoxljfisddspjkolvmsihoajpḱfðlsðfkdoilfsæklajfdgopilhjkgnlfæsdfkoaijrhknljisdfklgdkljdfskjldsfkjlsdkjldfskjdflsdkljsdkjlskjldfkjlseiojijisdjlsefjlkslkjfskljfjsdofjsjdgijsdjigjiodfgiojdfgifigojifdigjidfijogdfijogijodgifoigdfojigdidgojigojisejioseojiseoijsfsiojfoijsfisfijiosojisjogigdjigdjogjiogsjoiggojaigajgjaklgkajlægajkljagkeopijaiogajgioajigæofdkjlægjdfgækfdlgjdfigiolskjoiklojilkojilksdfoijkldfjioksdfiojksfldijkllllllllllllllllllllllllllllllllllkldskjlsdfkjlkjlsdkjlfjsdkjlfjlksdflksdflkjsdljkfslkjdfjklsdkljfkjlsdjfklsjdkjkfsdjklfklsdfkjkjlsdjklfskdjlfkjlsddfslkjsdjklfkjl",
-        "title": "Blog post 2",
-        "shortDescription": "another cool blog post"
-      },
-      {
-        "id": "44593",
-        "thumbnail": {
-          "url": "https://www.datocms-assets.com/65130/1647527094-us-skateboarder-cory-juneau.jpg"
-        },
-        "createdAt": "2022-03-17T14:26:22Z",
-        "content": "Hellow world this is the first blog post lets get this stuff going hell yeah ![](https://www.datocms-assets.com/65130/1647527094-us-skateboarder-cory-juneau.jpg)\nthis is some more text about stuff and things im just writing with out a care in the world just rambling on and on to write more text to fill out this random blog post and there is nothing you can do to stoppp me heheheh lets continu tiping about every thing an nothing this is super fun what is going on lol more more lolololololololol",
-        "title": "Blog post 1",
-        "shortDescription": "Cool blog post about stuff and things"
-      },
-      {
-        "id": "44609",
-        "thumbnail": {
-          "url": "https://www.datocms-assets.com/65130/1647527094-us-skateboarder-cory-juneau.jpg"
-        },
-        "createdAt": "2022-03-17T14:38:59Z",
-        "content": "ksadklasjdkljaskljdkjlsalkdkjsfkjlsdfjlsklklslksfjlksdflksfjklsjfsklnlksfjkls",
-        "title": "blog post 3 ",
-        "shortDescription": ""
+async function fetchData(id:any){
+  const datoToken = '45d3feff8f1aa49044bf21b6aab5bc'
+  const datoUrl = 'https://graphql.datocms.com/'
+  const datoQuery = `
+  {
+    post(filter: {id: {eq: ${id}}}) {
+      title
+      post {
+        links
+        value
+        blocks {
+          image {
+            url
+            id
+          }
+          id
+        }
       }
-    ]
+      id
+      createdAt
+    }
+  }`
+
+  const datoRequest = await fetch(datoUrl, {
+    method: 'POST',
+    body: JSON.stringify({ query: datoQuery }),
+    headers: {
+      Authorization: datoToken,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+
+  if (!datoRequest.ok) {
+    throw new Error(`HTTP error! Status: ${datoRequest.status}`)
+  }
+
+  const datoCMSResponse: any = await datoRequest.json()
+
+return await datoCMSResponse
+}
 
 export async function get({ params }) {
-  
-
-  let item = await mockdata.find(post => post.id == params.post)
-  
-   return {body:{item}} 
+   return {body: await fetchData(params.post)} 
 }
