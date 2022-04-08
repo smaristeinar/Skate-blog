40e0530fd4f427d9a37991a3db28528d531e3445
smaristeinar <smairkul@hotmail.com>
2022-04-08 13:54:43 +0200

    feat: date, time format

--- frontend/src/routes/post/[id].svelte
+++ frontend/src/routes/post/[id].svelte
@@ -16,7 +16,22 @@
 <script lang="ts">
 import { render as renderer } from 'datocms-structured-text-to-html-string'
 export let article;
-console.log(article);
+
+const formatDate = (date) => {
+  let d = new Date(date);
+  let month = (d.getMonth() + 1).toString();
+  let day = d.getDate().toString();
+  let year = d.getFullYear();
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+  return [year, month, day].join('-');
+}
+console.log(formatDate('Febuary 1, 2021'));
+
 
 const options = {
   renderBlock({ record, adapter: { renderNode } }) {
@@ -24,13 +39,15 @@ const options = {
   }
 };
 
+
 console.log(renderer(article.data.post.post, options));
 </script>
 
 
 <article>
-<div>
- {article.data.post.createdAt} 
+<div class="title">
+<h1>{article.data.post.title}</h1>
+ <p>{formatDate(article.data.post.createdAt)} | SkateBlog </p>
 </div>
 <div class="content">
 {@html renderer(article.data.post.post, options)}
@@ -45,6 +62,14 @@ font-family: "NotoSans";
 
 .content :global(img){
   max-width: 100%;
+}
+
+.title{
+padding-bottom: 10px;
+}
+.title > h1{
+  font-size: var(--text-5xl);
+  padding-bottom: 5px;
 }
 
 article{
