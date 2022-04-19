f5f5e41b8153c41a9741d5c105c56c83ffdc26b0
smaristeinar <smairkul@hotmail.com>
2022-04-19 12:32:27 +0200

    test

--- frontend/src/routes/api/getThumbnails.ts
+++ frontend/src/routes/api/getThumbnails.ts
@@ -2,7 +2,7 @@ export async function get({platform}) {
       let tumbNails = []
       const values = await platform.env.TUMBNAILS.list();
      await values.keys.forEach(async tumbnail => {
-       tumbNails.push(await platform.env.THUMBNAILS.get(tumbnail, {type: "json"}))
+       tumbNails.push(await platform.env.THUMBNAILS.get(tumbnail.name, {type: "json"}))
       });
 
      console.log(values);
