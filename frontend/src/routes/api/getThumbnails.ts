6b517c6b3e8ed2df03d887eb192b1c9bde9edeed
smaristeinar <smairkul@hotmail.com>
2022-04-19 16:40:26 +0200

    test

--- frontend/src/routes/api/getThumbnails.ts
+++ frontend/src/routes/api/getThumbnails.ts
@@ -1,42 +1,6 @@
-async function getThumbnails(keys, {platform}){
-let thumbnails = []
-await keys.forEach(async tumbnail => {
-      let thumbnailData = await platform.env.TUMBNAILS.get(tumbnail.name, {type: "json"})
-       thumbnails.push(await thumbnailData)
-      })
-      return thumbnails
-}
 
 export async function get({platform}) {
-  // const values = await platform.env.TUMBNAILS.list();
-  
-   let list = [
-    {
-      "name": "THE SQUAD | A Video By Calvin Millar"
-    },
-    {
-      "name": "another skate article"
-    },
-    {
-      "name": "more aabout this skate articleaaaaaaaaaaaaaaaaaaaa"
-    },
-    {
-      "name": "skate community and beyond, Nata"
-    }
-  ]
-
-/*   await values.keys.forEach(async (element:any, {platform}) => {
-   let key = element.name
-   let item = platform.env.TUMBNAILS.get(await key, { type: "json" })
-   items.push(await item)
-   });  
-*/
-
-
-   return { body: platform.env.TUMBNAILS.get("thumbnails", { type: "json" })}
-
+   let thumbnails = await platform.env.TUMBNAILS.get("thumbnails", { type: "json" })
+   return { body: await thumbnails }
 }
  
-
-  /**platform.env.TUMBNAILS.get(values.keys[0].name, { type: "json" })
- */
