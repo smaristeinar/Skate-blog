f2f9b3c47da9943fd15a116f5e2beda96d663347
smaristeinar <smairkul@hotmail.com>
2022-04-19 12:21:05 +0200

    test

--- frontend/src/routes/api/getThumbnails.ts
+++ frontend/src/routes/api/getThumbnails.ts
@@ -1,4 +1,5 @@
 export async function get({platform}) {
-     const values = await platform.env.THUMBNAIL.list();
+     const values = await platform.env.TUMBNAILS.list();
+     console.log(values);
      return {body: await values} 
   }
