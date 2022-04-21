4b8c0204766812582855de4875d0e7ee1a3d4989
smaristeinar <smairkul@hotmail.com>
2022-04-21 13:06:18 +0200

    cleanup

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,9 +1,7 @@
 <script context="module">
  export async function load({fetch}) {
-    console.log("loading");
     const url = "/api/getThumbnails";
     const response = await fetch(url);
-    console.log(response);
     return {
       props: {
         article: await response.json()
@@ -16,7 +14,6 @@
 import FeaturePost from "../components/FeaturePost.svelte";
 import Posts from "../components/Posts.svelte"; 
 export let article:any
-console.log(article);
 </script>
 
 <FeaturePost post={article.data.allPosts[0]}/>
